23a8f7da09d41c05cda2907abc904182be6b7f36
Jerry Cui <125308969+jerrycui2007@users.noreply.github.com>
2024-09-16 18:00:14 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * [Main.java]
+ * Initializes the <code>TextEditor</code> object
+ *
+ * @author Jerry Cui
+ * @version 1.0
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        new TextEditor();
+    }
+}
